5e7e658508250d6374bb690f26f1422615232d72
mrmücahit <mucahitarpaci34@gmail.com>
2021-10-06 19:48:35 +0300

    ilk github projem

---
+++ döngüler ve diziler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace döngüler_ve_diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
